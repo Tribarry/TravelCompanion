4884432af0955862146b80bdc7a5f3add4e15a5b
Tribarry <tristanbarilari@gmail.com>
2026-09-12 07:03:28 +1000

    Bump cache for Vietnam photo defect fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sea2027-v6-local-vietnam';
+const CACHE='sea2027-v7-vietnam-photo-fixes';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./data/itinerary.json','./manifest.webmanifest','./data/vietnam-photo-manifest-v1.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
